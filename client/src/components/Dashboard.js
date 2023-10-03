1fae6e3d00819050b901c2a50ba544b340295914
Naman Agarwal <naman.ag41402@gmail.com>
2023-10-03 19:07:45 +0530

    footer added on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,19 +2,21 @@ import React, { Fragment } from "react";
 import SideNavbar from "./SideNavbar";
 import CreateForm from "./create post/CreateForm";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Dashboard = () => {
   return (
     <Fragment>
       <div className="create">
-        {/* <div className="left-create"> */}
-        <SideNavbar />
-        {/* </div> */}
+        <div className="left-create">
+          <SideNavbar />
 
-        <div className="right-create">
-          <Navbar />
-          <CreateForm />
+          <div className="right-create">
+            <Navbar />
+            <CreateForm />
+          </div>
         </div>
+        <Footer />
       </div>
     </Fragment>
   );
